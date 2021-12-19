256e2848a6d70a44f14ae0f98a51de1de6898104
Abdulmajeed Alduaifi <438101329@student.ksu.edu.sa>
2021-12-19 18:00:24 +0300

    fix bug

--- frontend/src/pages/NewPost.js
+++ frontend/src/pages/NewPost.js
@@ -66,7 +66,7 @@ function NewPost() {
   }
   useEffect(() => {
     const reloadCount = sessionStorage.getItem("reloadCount");
-    if (reloadCount < 2) {
+    if (reloadCount < 1) {
       sessionStorage.setItem("reloadCount", String(reloadCount + 1));
       window.location.reload();
     } else {
